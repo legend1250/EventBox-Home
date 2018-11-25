fd89c9727442f583c75718a36f90db6bb7f6b470
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2018-11-25 22:21:12 +0700

    tune style list

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Grid } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 @inject('stores')
 @observer
 class EventList extends Component{
 
+  chunkArray(myArray, chunkSize){
+    var results = []
+    while (myArray.length) {
+      results.push(myArray.splice(0, chunkSize))
+    }
+    
+    return results
+  }
+
   render(){
     const { events } = this.props.stores.event
     const { edges } = events
 
     return(
-      edges && edges.map(item => (
-        <EventItem key={item.id} event={item}  />
-      ))
+      <Grid columns={3} >
+        {this.chunkArray(edges.toJS(), 3).map((item, index) => (
+          <Grid.Row key={index} >
+            {item && item.map(event => (
+              <Grid.Column key={event.slug} >
+                <EventItem event={event} />
+              </Grid.Column>
+            ))}
+          </Grid.Row>
+        ))}
+      </Grid>
     )
   }
 }
@@ -23,9 +40,9 @@ export default EventList
 
 const EventItem = ({event}) => (
   <Card>
-    <Image src={event.images.thumbnail} />
+    <Image src={event.images.thumbnail} style={{height: 192}} />
     <Card.Content>
-      <Card.Header>
+      <Card.Header style={{height: 46}} >
         <Link to={`/event/${event.slug}-${event.id}`} target="_blank">
           {event.title}
         </Link>
@@ -33,7 +50,7 @@ const EventItem = ({event}) => (
       <Card.Meta>
         <span className='date'>CreatedAt: {new Date(Number(event.createdAt)).toLocaleString()}</span>
       </Card.Meta>
-      <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+      <Card.Description style={{height: 58}} >{event.shortDescription}</Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
--- webpack.client.js
+++ webpack.client.js
@@ -5,6 +5,7 @@ const merge          = require('webpack-merge')
 const baseConfig     = require('./webpack.base.js')
 
 const config = {
+  mode: 'development',
   entry: './src/client.js',
   resolve: {
     modules: [
